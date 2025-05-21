76fbc72f3aa2d3d07d8330b50febc75a2cec4684
squid <squid@stormwind.it>
2025-05-21 17:11:31 +0200

    chore(run_aot.sh): refactor script to improve readability and add support for passing arguments to the AOT-compiled server script

--- scripts/run_aot.sh
+++ scripts/run_aot.sh
@@ -1,1 +1,6 @@
-dotnet publish -r osx-arm64 -o dist -p:PublishAot=true -c Release src/Moongate.Server && ./dist/Moongate.Server && rm -Rf moongate && rm -Rf dist
+#!/bin/bash
+
+dotnet publish -r osx-arm64 -o dist -p:PublishAot=true -c Release src/Moongate.Server \
+  && ./dist/Moongate.Server "$@" \
+  && rm -Rf moongate \
+  && rm -Rf dist
