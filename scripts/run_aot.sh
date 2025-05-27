b28293613f0cb808841dc4d44ba8f85bdb0afd16
squid <squid@stormwind.it>
2025-05-27 13:53:49 +0200

    feat(data): add body type table configuration file for different body types feat(data): add support for various body types such as Monster, Sea, Animal, Human, Equipment feat(data): include additional equipment and creatures for different game versions and fixes

--- scripts/run_aot.sh
+++ scripts/run_aot.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-dotnet publish -r osx-arm64 -o dist -p:PublishAot=true -c Debug src/Moongate.Server \
+RELEASE="Debug"
+
+dotnet publish -r osx-arm64 -o dist -p:PublishAot=true -c $RELEASE src/Moongate.Server \
   && ./dist/Moongate.Server "$@" \
   && rm -Rf moongate \
   && rm -Rf dist
